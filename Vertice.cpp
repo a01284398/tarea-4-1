80b47e7a673d0835803288c660f285ecd79c01fc
a01284398 <84676132+a01284398@users.noreply.github.com>
2021-11-11 10:24:54 -0600

    Update Vertice.cpp

--- Vertice.cpp
+++ Vertice.cpp
@@ -1,1 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Vertice{
+  private:
+  int id;
+  int dato;
+  static int counter;
+
+  public:
+  Vertice(){
+    dato=0;
+    id=0;
+  }
+  Vertice(int id, int dato){
+    this->dato=dato;
+    this->id=id;
+  }
+
+  Vertice(int dato){
+    this->dato=dato;
+    int id=0;
+  }
+
+  void setId(int id){
+    this-> id = id;
+  }
+  int getId(){
+    return id;
+  }
+  void setDato(int dat){
+    dato = dat;
+  }
+  int getDato(){
+    return dato;
+  }
+};
 
