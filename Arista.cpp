5540497b9996a6218cb11e6a753280267583eb6d
a01284398 <84676132+a01284398@users.noreply.github.com>
2021-11-11 10:25:24 -0600

    Update Arista.cpp

--- Arista.cpp
+++ Arista.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
+#include "Vertice.h"
 using namespace std;
 
-class Arista{
+class Arista
+{
   private:
-  int vi, vj;
+    Vertice vi;
+    Vertice vj;
+
   public:
-  void setVi();
-  int getVi();
-  void setVj();
-  int getVj();
+    void setVi(Vertice vi)
+    {
+      this->vi = vi;
+    }
+
+   Vertice getVi()
+    {
+     return vi;
+    }
+
+    void setVj(Vertice vj)
+    {
+      this->vj = vj;
+    }
+
+    Vertice getVj()
+    {
+      return vj;
+   }
 };
